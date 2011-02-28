890d69e60410314146911b82bec067b472bbc03f
Mike Hirth <hirthy@gmail.com>
2011-02-27 20:57:03 -0500

    Rearranged comments.

--- pitchshift.m
+++ pitchshift.m
@@ -20,30 +20,29 @@ frameLength = 512;                      % frame length
 overlap = .75;                          % overlap fraction
 window = hanning(frameLength)';         % input window
 
-inputLength = length(input);           % length of input signal
-numFrames = floor((inputLength-2*frameLength)/(frameLength*(1-overlap)));
-                                        % number of frames in input
-Ra = floor(frameLength*(1-overlap));    % analysis time hop
-Rs = floor(alpha*Ra);                   % synthesis time hop
-Wk = (0:(frameLength-1))*2*pi/frameLength;       % center bin frequencies
-output = zeros(1, inputLength*alpha);  % output signal initialization
+inputLength = length(input);                                              % length of input signal
+numFrames = floor((inputLength-2*frameLength)/(frameLength*(1-overlap))); % number of frames in input
+Ra = floor(frameLength*(1-overlap));                                      % analysis time hop
+Rs = floor(alpha*Ra);                                                     % synthesis time hop
+Wk = (0:(frameLength-1))*2*pi/frameLength;                                % center bin frequencies
+output = zeros(1, inputLength*alpha);                                     % output signal initialization
 
-XCurr = fft(window.*input(1:frameLength)); % analyze initial frame
-phiYCurr = angle(XCurr);       % initial frame output phases
+XCurr = fft(window.*input(1:frameLength));  % analyze initial frame
+phiYCurr = angle(XCurr);                    % initial frame output phases
 
 for i=1:numFrames
-    XPrev = XCurr;                  % store last frame's STFT
-    phiYPrev = phiYCurr;            % store last frame's output phases
-    XCurr = fft(window.*input(i*Ra:i*Ra+frameLength-1)); % analyze current frame
-    DPhi = angle(XCurr) - angle(XPrev) - Ra*Wk; % unwrapped phase change
-    DPhip = mod(DPhi+pi, 2*pi) - pi;    % principle determination (+/- pi)
-    wHat = Wk + (1/Ra)*DPhip;         % estimated "real" bin frequency
-    phiYCurr = phiYPrev + Rs*wHat;     % Phase propagation formula
-    Yu = abs(XCurr).*exp(j*phiYCurr);   % output STFT
+    XPrev = XCurr;                                                                      % store last frame's STFT
+    phiYPrev = phiYCurr;                                                                % store last frame's output phases
+    XCurr = fft(window.*input(i*Ra:i*Ra+frameLength-1));                                % analyze current frame
+    DPhi = angle(XCurr) - angle(XPrev) - Ra*Wk;                                         % unwrapped phase change
+    DPhip = mod(DPhi+pi, 2*pi) - pi;                                                    % principle determination (+/- pi)
+    wHat = Wk + (1/Ra)*DPhip;                                                           % estimated "real" bin frequency
+    phiYCurr = phiYPrev + Rs*wHat;                                                      % Phase propagation formula
+    Yu = abs(XCurr).*exp(j*phiYCurr);                                                   % output STFT
     output(i*Rs:i*Rs+frameLength-1) = output(i*Rs:i*Rs+frameLength-1) + real(ifft(Yu)); % add current frame to output
 end
 
-outputNorm = output./max(output);      % normalize the output amplitude
+outputNorm = output./max(output);       % normalize the output amplitude
 [t,d]=rat(alpha);                       % determine integer shift ratio
 shifted = resample(output,d,t);         % resample for pitch shift
 
